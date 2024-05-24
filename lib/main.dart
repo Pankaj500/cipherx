826dcaef4839bde6ac493ddd981831fa4052ae01
Pankaj Kumar Rao <135265653+Pankaj500@users.noreply.github.com>
2024-05-24 20:36:47 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:cipherx/features/splash_screen.dart';
-import 'package:cipherx/firebase/homepage.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
